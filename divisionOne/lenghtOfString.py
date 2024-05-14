1964897d44aa1a4941bb3061d46bb0a7648a4c81
Akaabiam Richards <richardsakaabiam@gmail.com>
2024-05-14 08:55:20 +0100

    updated

--- divisionOne/lenghtOfString.py
+++ divisionOne/lenghtOfString.py
@@ -1,7 +1,9 @@
 print("Your task is to get the total number of characters in a name")
 # print(f"The total is {len(input('What is your name? '))}")
-print(f"Your name has a total of {len(input('What is your name?'))} characters!")
+# print(f"Your name has a total of {len(input('What is your name?'))} characters!")
 
-
-
-
+print("Welcome to python variables")
+name = input("What is your name?\n")
+length = len(name)
+whiteSpace = name.count(" ")
+print(f"The total characters are {length - whiteSpace}")
