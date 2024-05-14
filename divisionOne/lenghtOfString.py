05f46b4a3dd1eaa64cd08db42cf26f0a96a86b65
Akaabiam Richards <richardsakaabiam@gmail.com>
2024-05-14 14:30:12 +0100

    updated

--- divisionOne/lenghtOfString.py
+++ divisionOne/lenghtOfString.py
@@ -5,12 +5,20 @@ print("Your task is to get the total number of characters in a name")
 print("Welcome to python variables")
 name = input("What is your name?\n")
 length = len(name)
-whiteSpace = name.count(" ")
-print(f"The total characters are {length - whiteSpace}")
-a = input("a:")
-b = input("b:")
-c = a
-a = b
-b = c
-print("a = " + a)
-print("b = " + b)
+# whiteSpace = name.count(" ")
+# print(f"The total characters are {length - whiteSpace}")
+# a = input("a:")
+# b = input("b:")
+# c = a
+# a = b
+# b = c
+# print("a = " + a)
+# print("b = " + b)
+name = input("Enter Your Name")
+print("HELLO! " + name)
+print("Enter the name of your CITY")
+city = input("City name\n ")
+print("Enter the name of your PET")
+pet = input("Pet name\n ")
+print("The name of your band is  " + city + " " + pet)
+
