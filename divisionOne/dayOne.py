e458a4946834e67517c846fff66596a31daa2c68
Akaabiam Richards <richardsakaabiam@gmail.com>
2024-05-14 08:15:18 +0100

    first commit

---
+++ divisionOne/dayOne.py
@@ -0,0 +1,19 @@
+print(" Hello Ritch! \n welcome to your new era of python ")
+print(" Hi" + " Chris")
+print("Day 1" + " - " + "Python Print Function")
+print("Day 1 - Python print Function")
+print("The function is written like this")
+print("print('What to print')")
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with + sign.")
+print('e.g print("Hello" + "world)")')
+print("New lines can be created with a backslash and n.")
+print("INTRODUCING THE INPUT FUNCTION")
+print("when using the input, Enter your prompt here!!")
+input("What is your name? ")
+print("Hello " + input(" What is your name?"))
+
+
+
+
+
---
+++ divisionOne/lenghtOfString.py
@@ -0,0 +1,7 @@
+print("Your task is to get the total number of characters in a name")
+# print(f"The total is {len(input('What is your name? '))}")
+print(f"The total is {len(input('What is your name? '))}")
+
+
+
+
