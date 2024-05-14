8c9a8f1284819e1598037c36a582c0e30ea5636d
Akaabiam Richards <richardsakaabiam@gmail.com>
2024-05-14 23:41:50 +0100

    updated

--- divisionOne/lenghtOfString.py
+++ divisionOne/lenghtOfString.py
@@ -23,9 +23,18 @@
 # pet = input("Pet name\n ")
 # print("The name of your band is  " + city + " " + pet)
 
-print("Welcome, input two numbers")
-two_digit_number = input("Type a two digit number\n")
-first_num = int(two_digit_number[0])
-second_num = int(two_digit_number[1])
-result = int(first_num + second_num)
-print(f"The sum of the two digits is {result}")
+# print("Welcome, input two numbers")
+# two_digit_number = input("Type a two digit number\n")
+# first_num = int(two_digit_number[0])
+# second_num = int(two_digit_number[1])
+# result = int(first_num + second_num)
+# print(f"The sum of the two digits is {result}")
+
+print("Welcome to the weeks calculator")
+age = input("Welcome, What is your current Age? ")
+age_as_int = int(age)
+years_remaining = 90 - age_as_int
+days = years_remaining * 365
+weeks = years_remaining * 52
+months = years_remaining * 12
+print(f"Your have {days} days {weeks} weeks and {months} months left")
